9553561c4d40ed7a790fe6da3510adb189e88448
Daniel Thurau <daniel.n.thurau@gmail.com>
2021-05-25 10:32:32 -0700

    Add Google Analytics plugin integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,12 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-8Y5H1N5QF7"],
+      },
+    },
   ],
 }
